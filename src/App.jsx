43d6cb4dc2ce6df6b95ce5a31170b69f79d51004
BrianEP-hub <54816472+BrianEP-hub@users.noreply.github.com>
2022-10-03 22:09:13 +0200

    readded container to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,21 @@ import React from 'react';
 import { Footer, Navbar } from './components';
 import { Home, Admin, Rooms, Contact } from './pages';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Container } from '@mui/material';
 
 const App = () => {
 	return (
 		<>
 			<Router>
 				<Navbar />
-				<Routes>
-					<Route index element={<Home />} />
-					<Route path="/admin" element={<Admin />} />
-					<Route path="/rooms" element={<Rooms />} />
-					<Route path="/contact" element={<Contact />} />
-				</Routes>
+				<Container maxWidth="lg">
+					<Routes>
+						<Route index element={<Home />} />
+						<Route path="/admin" element={<Admin />} />
+						<Route path="/rooms" element={<Rooms />} />
+						<Route path="/contact" element={<Contact />} />
+					</Routes>
+				</Container>
 			</Router>
 			<Footer />
 		</>
