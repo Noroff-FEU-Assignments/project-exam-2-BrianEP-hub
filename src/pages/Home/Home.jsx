c2736629c36ac01c8c04684c47e80163089c33d4
BrianEP-hub <54816472+BrianEP-hub@users.noreply.github.com>
2022-10-06 22:32:43 +0200

    sectioned home.jsx

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,18 @@
+import { Typography } from '@mui/material';
 import React from 'react';
 import { ImageSlider } from '../../components';
 
 const Home = () => {
 	return (
 		<>
-			<ImageSlider />
+			<div>
+				<ImageSlider />
+			</div>
+			<div>
+				<Typography variant="h4">
+					This is the header of the home page
+				</Typography>
+			</div>
 		</>
 	);
 };
