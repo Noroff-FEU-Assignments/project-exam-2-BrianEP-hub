3a2f195ef2efe04ec5b0400b2513d338032d3158
BrianEP-hub <54816472+BrianEP-hub@users.noreply.github.com>
2022-10-10 18:08:13 +0200

    Prelim work for payment component

--- src/components/Stepper/Payment.jsx
+++ src/components/Stepper/Payment.jsx
@@ -1,8 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Container, Paper, Stepper } from '@mui/material';
 
 const Payment = () => {
+
+  const [checkoutStep, setCheckoutStep] = useState()
+
   return (
-    <div>Payment</div>
+    <div className="checkout">
+      <Container>
+        <Paper elevation={3}>
+          <Typography align="center" variant="h5" gutterBottom>
+            Checkout
+          </Typography>
+          {checkoutStep !== 'confirmation' && (
+            <Stepper
+             activeStep={steps.indexOf(checkoutStep)}>
+              {steps.map((label) => (
+                <Step key={label}>
+                  <StepLabel>{label}</StepLabel>
+                </Step>
+              ))}
+             </Stepper>
+          )}
+        </Paper>
+      </Container>
+    </div>
   )
 }
 
