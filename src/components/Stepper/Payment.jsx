351afaa61c528da21ee1b43117bb10a9c0d798c9
BrianEP-hub <54816472+BrianEP-hub@users.noreply.github.com>
2022-10-10 18:55:02 +0200

    more prelim work for stepper

--- src/components/Stepper/Payment.jsx
+++ src/components/Stepper/Payment.jsx
@@ -1,31 +1,38 @@
-import React, { useState } from 'react'
-import { Container, Paper, Stepper } from '@mui/material';
+import React, { useState } from 'react';
+import {
+	Container,
+	Paper,
+	Typography,
+	Stepper,
+	Step,
+	StepLabel,
+} from '@mui/material';
+
+const steps = ['order-info', 'order-details', 'order-confirmation'];
 
 const Payment = () => {
+	const [checkoutStep, setCheckoutStep] = useState();
 
-  const [checkoutStep, setCheckoutStep] = useState()
+	return (
+		<div className="checkout">
+			<Container>
+				<Paper elevation={3}>
+					<Typography align="center" variant="h5" gutterBottom>
+						Checkout
+					</Typography>
+					{checkoutStep !== 'confirmation' && (
+						<Stepper activeStep={steps.indexOf(checkoutStep)}>
+							{steps.map(label => (
+								<Step key={label}>
+									<StepLabel>{label}</StepLabel>
+								</Step>
+							))}
+						</Stepper>
+					)}
+				</Paper>
+			</Container>
+		</div>
+	);
+};
 
-  return (
-    <div className="checkout">
-      <Container>
-        <Paper elevation={3}>
-          <Typography align="center" variant="h5" gutterBottom>
-            Checkout
-          </Typography>
-          {checkoutStep !== 'confirmation' && (
-            <Stepper
-             activeStep={steps.indexOf(checkoutStep)}>
-              {steps.map((label) => (
-                <Step key={label}>
-                  <StepLabel>{label}</StepLabel>
-                </Step>
-              ))}
-             </Stepper>
-          )}
-        </Paper>
-      </Container>
-    </div>
-  )
-}
-
-export default Payment
+export default Payment;
