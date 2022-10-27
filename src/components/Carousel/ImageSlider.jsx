31fc2921c81e450c8de3e59e3bf24ac4ccc07aa1
BrianEP-hub <54816472+BrianEP-hub@users.noreply.github.com>
2022-10-27 22:28:49 +0200

    Prelim work for fetching images for carousel

--- src/components/Carousel/ImageSlider.jsx
+++ src/components/Carousel/ImageSlider.jsx
@@ -1,11 +1,11 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Carousel from 'react-material-ui-carousel';
 import { Paper } from '@mui/material';
 import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
 import ChevronRightIcon from '@mui/icons-material/ChevronRight';
 import styles from './carousel.module.scss';
 
-const items = [
+/* const items = [
 	{
 		name: 'Randoman #1',
 		description: 'The randomest man in the universe',
@@ -14,29 +14,55 @@ const items = [
 		name: 'Randoman #2',
 		description: 'Even more random than the first one!',
 	},
-];
+]; */
 
 const ImageSlider = () => {
+	useEffect(() => {
+		getHero();
+	}, []);
+
+	const [hero, setHero] = useState([]);
+
+	const getHero = async () => {
+		const options = {
+			method: 'GET',
+			headers: {
+				Accept: '*/*',
+				'Content-type': 'application/json',
+			},
+		};
+		try {
+			const resp = await fetch(
+				`${process.env.REACT_APP_HERO_URL}?populate=*`,
+				options,
+			);
+			const data = await resp.json();
+
+			setHero(data.data);
+			console.log(data.data);
+		} catch (error) {
+			console.error(error);
+		}
+	};
+
 	return (
 		<Carousel
 			className={styles.carousel}
 			NextIcon={<ChevronRightIcon />}
 			PrevIcon={<ChevronLeftIcon />}
 		>
-			{items.map((item, i) => (
-				<Item key={i} item={item} />
-			))}
+			<Paper className={styles.paper}>
+				<img
+					src={hero.hero_banner}
+					alt={hero.hero_alt_text}
+				/>
+			</Paper>
 		</Carousel>
 	);
 };
 
 export default ImageSlider;
 
-const Item = props => {
-	return (
-		<Paper className={styles.paper}>
-			<h2>{props.item.name}</h2>
-			<p>{props.item.description}</p>
-		</Paper>
-	);
-};
+/* const Item = props => {
+	return <Paper className={styles.paper}></Paper>;
+}; */
